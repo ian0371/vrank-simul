76558cfdb9ff972fcc3479f86aff7b8da2bdbe35
Chihyun Song <ian0371@gmail.com>
2023-06-29 15:24:37 +0900

    vrank simul with csv output

--- script/vrank.ts
+++ script/vrank.ts
@@ -1,52 +1,133 @@
 import { ethers } from "hardhat";
 import { Header, commiterAddress, parseExtraData } from "../lib/hardhat/utils";
+import * as fs from "fs";
 
 const period = 3600;
-let council: string[] = [];
+const alpha = 0.2;
+let council: string[];
+let blockRecords: BlockRecord[] = [];
 
-type Block = {
+type BlockRecord = {
   blockNum: number;
-  committers: string[];
+  voters: string[];
+  nonvoters: string[];
 };
 
-type Blocks = {
-  [blockNum: number]: Block;
-};
-
-type ValidatorScore = {
-  nonVoteCnt: number;
-  maxNonVoteCntCons: number;
-  voteAdd: number;
-  voteDeduct: number;
-  voterScore: number;
-};
-
-type Scores = {
-  blockNum: number;
-} & {
-  [address: string]: ValidatorScore;
-};
-
-/*
-function apply(block: Block, scores: Scores): Scores {
-  return null;
+async function getCouncil(blockNum: number): Promise<string[]> {
+  const nonChecksum: string[] = await ethers.provider.send("klay_getCouncil", [blockNum]);
+  // convert to checksum-addresses
+  return nonChecksum.map(ethers.utils.getAddress);
 }
-*/
 
-async function getHeader(blockNum: number) {
+function nonVoteCnt(slot: number, addr: string) {
+  let ret = 0;
+  for (let i = 0; i <= slot; i++) {
+    const record = blockRecords[i];
+    if (record.nonvoters.includes(addr)) {
+      ret++;
+    }
+  }
+  return ret;
+}
+
+function findLongestContSubseqLen(arr: number[]): number {
+  if (arr.length == 0) {
+    return 0;
+  }
+
+  let maxLength = 1;
+  let currentLength = 1;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - 1 == arr[i - 1]) {
+      currentLength++;
+    } else {
+      maxLength = Math.max(maxLength, currentLength);
+      currentLength = 1;
+    }
+  }
+
+  maxLength = Math.max(maxLength, currentLength);
+  return maxLength;
+}
+
+function voted(slot: number, addr: string) {
+  const record = blockRecords[slot];
+  if (record.voters.includes(addr)) {
+    return true;
+  }
+  return false;
+}
+
+function maxNonVoteCntCons(slot: number, addr: string) {
+  const nonvotedList = [];
+  for (let i = 0; i <= slot; i++) {
+    const record = blockRecords[i];
+    if (!voted(slot, addr)) {
+      nonvotedList.push(record.blockNum);
+    }
+  }
+  return findLongestContSubseqLen(nonvotedList);
+}
+
+function voteAdd(slot: number, addr: string) {
+  return (50 * slot) / period;
+}
+
+function voteDeduct(slot: number, addr: string) {
+  return nonVoteCnt(slot, addr) * alpha + Math.floor(maxNonVoteCntCons(slot, addr) * 2 * alpha);
+}
+
+function voterScore(slot: number, addr: string) {
+  return 50 + voteAdd(slot, addr) - voteDeduct(slot, addr);
+}
+
+async function getRecord(blockNum: number): Promise<BlockRecord> {
   const header: Header = await ethers.provider.send("klay_getHeaderByNumber", [blockNum]);
   const extraData = parseExtraData(header.extraData);
+  const record: BlockRecord = { blockNum, voters: [], nonvoters: [] };
   for (let i = 0; i < extraData.committedSeal.length; i++) {
-    console.log(commiterAddress(header, i));
+    record.voters.push(commiterAddress(header, i));
+  }
+  for (let i = 0; i < council.length; i++) {
+    if (!record.voters.includes(council[i])) {
+      record.nonvoters.push(council[i]);
+    }
+  }
+  return record;
+}
+
+// startBlock, endBlock both inclusive
+async function fillRecords(startBlock: number, endBlock: number) {
+  if (startBlock % period != 0) {
+    throw new Error("startBlock must be multiple of period");
+  }
+
+  for (let block = startBlock; block <= endBlock; block++) {
+    const record = await getRecord(block);
+    blockRecords.push(record);
   }
 }
 
 async function main() {
-  await getHeader(126008291);
-  // get council
-  // get block between [3600k, 3600(k+1)]
-  // ecrecover each block, and count
-  // apply vrank
+  const blockNum = 126007200;
+  council = await getCouncil(blockNum);
+  await fillRecords(blockNum, blockNum + period);
+
+  const fn = `output_${blockNum}.csv`;
+  fs.writeFileSync(fn, `addr`);
+  for (let i = 0; i <= period; i++) {
+    fs.appendFileSync(fn, `, ${blockNum + i}`);
+  }
+  fs.appendFileSync(fn, `\n`);
+
+  for (const addr of council) {
+    fs.appendFileSync(fn, `${addr}`);
+    for (let i = 0; i <= period; i++) {
+      fs.appendFileSync(fn, `,${voterScore(i, addr)}`);
+    }
+    fs.appendFileSync(fn, `\n`);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
